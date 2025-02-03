6009c4847699b66bcfc80887da3d41ab98ad9746
Joseph Scott <165531069+jjscott17@users.noreply.github.com>
2025-02-02 23:19:10 -0800

    Update TestCases.java
    Fixed typo

--- src/TestCases.java
+++ src/TestCases.java
@@ -13,7 +13,7 @@ public class TestCases {
         // Test Case 1: {{1,2,3},{5,6,7},{9,10,11}}
         int matrix1[][] = new int[][]{{1, 2, 3}, {5, 6, 7}, {9, 10, 11}};
         System.out.println("Test 1: "+SpiralMatrix.spiralOrder(matrix1));
-        System.out.println("Expected output: [1, 2, 3, 7, 11, 10, 9, 5, 6, 7]\n");
+        System.out.println("Expected output: [1, 2, 3, 7, 11, 10, 9, 5, 6]\n");
 
         // Test Case 2: {{1,2,3,4},{5,6,7,8},{9,10,11,12}}
         int matrix2[][] = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
